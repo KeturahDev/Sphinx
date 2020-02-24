941cf5ab5a87c6f2c69f054ed134648bca50da64
Student <example@example.com>
2020-02-24 10:34:33 -0800

    ask a riddle, return your answer

---
+++ Sphinx.cs
@@ -0,0 +1,27 @@
+using System;
+
+class Sphinx
+{
+
+  static void Main()
+  {
+    // ReadOutRiddle();
+    Console.WriteLine("The more of them you take, the more you leave behind. What are they?");
+
+    string userInput = Console.ReadLine();
+    Console.WriteLine(userInput);
+    // if (userInput == "footsteps" || userInput == "Footsteps")
+    // {
+    //   //give another riddle
+    // }
+    // else 
+    // {
+    //   Console.WriteLine("YOU DIED- Sphinx ate you");
+    // }
+  }
+
+  // static void ReadOutRiddle()
+  // {
+  //   Console.WriteLine("The more of them you take, the more you leave behind. What are they?");
+  // }
+}
