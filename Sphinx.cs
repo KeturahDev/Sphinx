9a1abe15551dbc7d504efe15cc4b7bbf1d9e2737
Student <example@example.com>
2020-02-24 10:38:51 -0800

    complete 3rd spec die if answer is wrong

--- Sphinx.cs
+++ Sphinx.cs
@@ -5,23 +5,21 @@ class Sphinx
 
   static void Main()
   {
-    // ReadOutRiddle();
-    Console.WriteLine("The more of them you take, the more you leave behind. What are they?");
-
+    ReadOutRiddle();
     string userInput = Console.ReadLine();
-    Console.WriteLine(userInput);
-    // if (userInput == "footsteps" || userInput == "Footsteps")
-    // {
-    //   //give another riddle
-    // }
-    // else 
-    // {
-    //   Console.WriteLine("YOU DIED- Sphinx ate you");
-    // }
+
+    if (userInput == "footsteps" || userInput == "Footsteps")
+    {
+      Console.WriteLine("YOU Didnt die- Sphinx will ask you anaother ridddle");
+    }
+    else 
+    {
+      Console.WriteLine("YOU DIED- Sphinx ate you");
+    }
   }
 
-  // static void ReadOutRiddle()
-  // {
-  //   Console.WriteLine("The more of them you take, the more you leave behind. What are they?");
-  // }
+  static void ReadOutRiddle()
+  {
+    Console.WriteLine("The more of them you take, the more you leave behind. What are they?");
+  }
 }
