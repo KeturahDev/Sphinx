c4406907a25c47980eed77f05b231fdd52c66f31
Student <example@example.com>
2020-02-24 16:30:54 -0800

    randomize questions and  allow ability to win after amount of questions asked

--- Sphinx.cs
+++ Sphinx.cs
@@ -4,93 +4,121 @@ using System.Collections.Generic;
 
 class Sphinx
 {
-  // Random rnd = new Random();
-
-  public static Dictionary<string, string> riddleOne = new Dictionary<string, string>() { {"Q", "The more of them you take, the more you leave behind. What are they?"}, {"A1", "footsteps"}, {"A2", "Footsteps"} };
-  
-  public static Dictionary<string, string> riddleTwo = new Dictionary<string, string>() { {"Q", "What is more useful when it is broken?"}, {"A1", "Egg"}, {"A2", "egg"} };
-  
-  public static Dictionary<string, string> riddleThree = new Dictionary<string, string>() { {"Q", "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?"}, {"A1", "Fire"}, {"A2", "fire"} };
-  
-  public static Dictionary<string, string> riddleFour = new Dictionary<string, string>() { {"Q", "What runs around the whole yard without moving?"}, {"A1", "Fence"}, {"A2", "fence"}, {"A3", "a fence"}};
-  
-  public static Dictionary<string, string> riddleFive = new Dictionary<string, string>() { {"Q", "What can you catch but never throw?"}, {"A1", "a cold"}, {"A2", "A cold"} };
-
-  // Dictionary<string, string>[] library = new Dictionary<string, string>[]
-  // {
-  // new Dictionary<string, string>() { {"Q", "The more of them you take, the more you leave behind. What are they?"}, {"A1", "footsteps"}, {"A2", "Footsteps"} },
-  
-  // new Dictionary<string, string>() { {"Q", "What is more useful when it is broken?"}, {"A1", "Egg"}, {"A2", "egg"} },
-  
-  // new Dictionary<string, string>() { {"Q", "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?"}, {"A1", "Fire"}, {"A2", "fire"} },
-  
-  // new Dictionary<string, string>() { {"Q", "What runs around the whole yard without moving?"}, {"A1", "Fence"}, {"A2", "fence"}, {"A3", "a fence"}},
-  
-  // new Dictionary<string, string>() { {"Q", "What can you catch but never throw?"}, {"A1", "a cold"}, {"A2", "A cold"} }
-  // }
-
+  public static int thisThing = 0;
   static void Main()
   {
-    Console.WriteLine("I AM SPHINX! ANSWER MY RIDDLES OR DIE");
-    Console.WriteLine(riddleOne["Q"]);
-    string userInput = Console.ReadLine();
+    Console.WriteLine("I AM SPHINX. ANSWER MY RIDDLES OR DIE");
+    Console.WriteLine(Play());
+    }
 
-    if (userInput == riddleOne["A1"] || userInput == riddleOne["A2"])
-    {
-      Console.WriteLine("You live... FOR NOW!");
-      Console.WriteLine(riddleTwo["Q"]);
-
-      string userInput2 = Console.ReadLine();
-
-      if (userInput2 == riddleTwo["A1"] || userInput2 == riddleTwo["A2"]) 
+  static string Play()
+  {
+    bool isCorrect = true;
+      while (isCorrect)   
       {
-        Console.WriteLine("You live... FOR NOW!"); 
-        Console.WriteLine(riddleThree["Q"]);
-
-        string userInput3 = Console.ReadLine();
-
-        if (userInput3 == riddleThree["A1"] || userInput3 == riddleThree["A2"]) 
+        if (thisThing <= 2)
         {
-          Console.WriteLine("You live... FOR NOW!"); 
-          Console.WriteLine(riddleFour["Q"]);
-                  
-          string userInput4 = Console.ReadLine();
-
-          if (userInput4 == riddleFour["A1"] || userInput4 == riddleFour["A2"]) 
-          {
-            Console.WriteLine("You live... FOR NOW!"); 
-            Console.WriteLine(riddleFive["Q"]);
-
-            string userInput5 = Console.ReadLine();
-
-            if (userInput5 == riddleFive["A1"] || userInput5 == riddleFive["A2"]) 
-            {
-              Console.WriteLine("You have passed my test, however you will still be my dinner Muahahaha!"); 
-            }
-            else 
-            {
-              Console.WriteLine("YOU DIED- Sphinx ate you");
-            }
-          }
-          else 
-          {
-            Console.WriteLine("YOU DIED- Sphinx ate you");
-          }
+          Console.WriteLine(thisThing);  
+          isCorrect = AskRiddle();
         }
         else 
         {
-          Console.WriteLine("YOU DIED- Sphinx ate you");
+          isCorrect = false;
+          return "AHHH... I've died. You win.";
         }
       }
-      else
-      {
-        Console.WriteLine("YOU DIED- Sphinx ate you");
-      }
-    }
-    else 
-    {
-      Console.WriteLine("YOU DIED- Sphinx ate you");
-    }
+      return "You lose.";
   }
 
+  static bool AskRiddle()
+  {
+    string[] riddles = {"The more of them you take, the more you leave behind. What are they?", "What is more useful when it is broken?", "I am not alive, but I grow; I don't have lungs, but I need air; I don't have a mouth, but water kills me. What am I?", "What runs around the whole yard without moving?", "What can you catch but never throw?"};
+    string[] response = {"footsteps", "egg", "fire", "fence", "a cold"};
+    Random rnd = new Random();
+    int randomIndex = rnd.Next(0,5);
+    Console.WriteLine(riddles[randomIndex]);
+    string userAnswer = Console.ReadLine();
+    if(userAnswer == response[randomIndex])
+    {
+      thisThing ++;
+      return true;
+
+    }
+    else
+    {
+      return false;
+    }
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // if (userInput == thisThing["A1"] || userInput == thisThing["A2"])
+    // {
+    //   Console.WriteLine("You live... FOR NOW!");
+      // Console.WriteLine(riddleTwo["Q"]);
+
+      // string userInput2 = Console.ReadLine();
+
+      // if (userInput2 == riddleTwo["A1"] || userInput2 == riddleTwo["A2"]) 
+      // {
+      //   Console.WriteLine("You live... FOR NOW!"); 
+      //   Console.WriteLine(riddleThree["Q"]);
+
+      //   string userInput3 = Console.ReadLine();
+
+    //     if (userInput3 == riddleThree["A1"] || userInput3 == riddleThree["A2"]) 
+    //     {
+    //       Console.WriteLine("You live... FOR NOW!"); 
+    //       Console.WriteLine(riddleFour["Q"]);
+                  
+    //       string userInput4 = Console.ReadLine();
+
+    //       if (userInput4 == riddleFour["A1"] || userInput4 == riddleFour["A2"]) 
+    //       {
+    //         Console.WriteLine("You live... FOR NOW!"); 
+    //         Console.WriteLine(riddleFive["Q"]);
+
+    //         string userInput5 = Console.ReadLine();
+
+    //         if (userInput5 == riddleFive["A1"] || userInput5 == riddleFive["A2"]) 
+    //         {
+    //           Console.WriteLine("You have passed my test, however you will still be my dinner Muahahaha!"); 
+    //         }
+    //         else 
+    //         {
+    //           Console.WriteLine("YOU DIED- Sphinx ate you");
+    //         }
+    //       }
+    //       else 
+    //       {
+    //         Console.WriteLine("YOU DIED- Sphinx ate you");
+    //       }
+    //     }
+    //     else 
+    //     {
+    //       Console.WriteLine("YOU DIED- Sphinx ate you");
+    //     }
+    //   }
+    //   else
+    //   {
+    //     Console.WriteLine("YOU DIED- Sphinx ate you");
+    //   }
+//     }
+//     else 
+//     {
+//       Console.WriteLine("YOU DIED- Sphinx ate you");
+//     }
+//   }
+
+// }
